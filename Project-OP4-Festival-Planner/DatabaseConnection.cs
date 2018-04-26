0c760aa57ab3720e6f92f75156f899d20b5a0a94
Sander Jochems <sanderjochems@hotmail.nl>
2018-04-26 14:54:09 +0200

    Update DatabaseConnection.cs

---
+++ Project-OP4-Festival-Planner/DatabaseConnection.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project_OP4_Festival_Planner
+{
+    class DatabaseConnection
+    {
+    }
+}
