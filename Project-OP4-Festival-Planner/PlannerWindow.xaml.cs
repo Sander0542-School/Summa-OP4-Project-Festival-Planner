1951ca50bd430d8221410d6ab96444278d4609bf
Sander Jochems <sanderjochems@hotmail.nl>
2018-04-26 15:11:33 +0200

    Update PlannerWindow.xaml.cs

--- Project-OP4-Festival-Planner/PlannerWindow.xaml.cs
+++ Project-OP4-Festival-Planner/PlannerWindow.xaml.cs
@@ -19,6 +19,8 @@ namespace Project_OP4_Festival_Planner
     /// </summary>
     public partial class PlannerWindow : Window
     {
+        private DatabaseConnection dbConnection = new DatabaseConnection();
+
         public PlannerWindow()
         {
             InitializeComponent();
