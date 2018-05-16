31a5e5cf91d47cde2730626c2fbb7f7eb5243715
Sander Jochems <sanderjochems@hotmail.nl>
2018-05-16 11:57:35 +0200

    Update ProgrammaDataWindow

--- Project-OP4-Festival-Planner/ProgrammaDataWindow.xaml.cs
+++ Project-OP4-Festival-Planner/ProgrammaDataWindow.xaml.cs
@@ -22,24 +22,45 @@ namespace Project_OP4_Festival_Planner
     {
         private DatabaseConnection dbConnection = new DatabaseConnection();
 
-        public ProgrammaDataWindow()
+        public ProgrammaDataWindow(string programmaID)
         {
             InitializeComponent();
 
-            LoadProgramma(1);
+            LoadProgrammas(programmaID);
         }
 
-        public void LoadProgramma(int programmaID)
+        public void LoadProgrammas(string programmaID)
         {
-            DataTable datatable = dbConnection.getProgrammaData(programmaID);
+            DataTable dataTableProgramma = dbConnection.getProgrammaData(programmaID);
 
-            lbProgramms.ItemsSource = datatable.DefaultView;
+            lbProgramms.ItemsSource = dataTableProgramma.DefaultView;
+
+            DataTable dataTablePodium = dbConnection.getPodiumInfo(programmaID);
+
+            tbPodiumNaam.Text = "Podium: " + dataTablePodium.Rows[0]["podiumNaam"].ToString();
+
+            tbGenres.Text = "Genres: " + dataTablePodium.Rows[0]["genres"].ToString();
+            tbOpbouwTijd.Text = "Opbouw Tijd: " + dataTablePodium.Rows[0]["opbouwTijd"].ToString();
+            tbAfbouwTijd.Text = "Afbouw Tijd: " + dataTablePodium.Rows[0]["afbouwTijd"].ToString();
+        }
+
+        public void LoadProgrammaData(string programmaInfoID)
+        {
+            DataTable dataTable = dbConnection.getProgrammaDataInfo(programmaInfoID);
+
+            tbBandNaam.Text = "Naam: " + dataTable.Rows[0]["bandNaam"].ToString();
+            tbBandGenre.Text = "Genre: " + dataTable.Rows[0]["bandGenre"].ToString();
+            tbBandKosten.Text = "Kosten: €" + dataTable.Rows[0]["bandPrijs"].ToString();
+
+            tbBeginTijd.Text = "Begin Tijd: " + dataTable.Rows[0]["beginTijd"].ToString();
+            tbEindTijd.Text = "Eind Tijd: " + dataTable.Rows[0]["eindTijd"].ToString();
         }
 
         private void lbProgramms_SelectionChanged(object sender, SelectionChangedEventArgs e)
         {
             ListBox listBox = (ListBox)sender;
-            MessageBox.Show(listBox.SelectedValuePath);
+
+            LoadProgrammaData(listBox.SelectedValue.ToString());
         }
     }
 }
